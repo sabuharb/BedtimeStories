9e87ecd115df53838a793ddd9dd885ee6c80914c
sabuharb <abuharb.sandy@gmail.com>
2024-10-08 10:37:29 -0500

    Updated it code

--- src/main/java/BedtimeStories.java
+++ src/main/java/BedtimeStories.java
@@ -1,32 +1,33 @@
-import javax.swing.text.StringContent;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BedtimeStories {
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Which story would you like to read? (goldilocks.txt, hansel_and_gretel.txt, mary_had_a_little_lamb.txt: ");
-        String fileName = input.nextLine();
-        
-        
-        File file = new File("Mod01/" + fileName);
-        
-        
+        BufferedReader reader;
+
         try {
-            Scanner fileReader = new Scanner(file);
+
+            System.out.println("Choose a file to read (goldilocks.txt, hansel_and_gretel.txt, mary_had_a_little_lamb.txt): ");
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = inputReader.readLine();
+
+
+            reader = new BufferedReader(new FileReader("Mod01/" + fileName));
+            String line;
             int lineNumber = 1;
-            while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
+
+
+            while ((line = reader.readLine()) != null) {
                 System.out.println(lineNumber + ": " + line);
                 lineNumber++;
             }
-            fileReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found. Please check the file name and try again.");
+
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
-
 }
